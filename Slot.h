925dcdd8f0f9c513d21f79b3a0aa683dcda29f26
Thea Olivia <theaolivia.mail@gmail.com>
2015-02-28 00:44:25 -0800

    Create Slot.h

---
+++ Slot.h
@@ -0,0 +1,16 @@
+#include <iostream>
+#include "library.h"
+#pragma once
+
+class Slot
+{
+	public:
+	// constructor, destructor
+	Slot(void);
+	~Slot(void);
+	// atribut
+
+	int Status[NSlot]; // Menyimpan data mengenai slot sementara
+	
+
+}
