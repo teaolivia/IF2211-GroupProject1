f20c421a6736584a607860c7a29651f496852ad5
Thea Olivia <theaolivia.mail@gmail.com>
2015-02-28 00:44:07 -0800

    Create Task.h

---
+++ Task.h
@@ -0,0 +1,15 @@
+#include <iostream>
+#include "library.h"
+#include "Slot.h"
+#include <fstream>
+#pragma once
+
+class Task
+{
+	public:
+	// constructor, destructor
+	Task(void);
+	~Task(void);
+	//atribut
+	
+}
