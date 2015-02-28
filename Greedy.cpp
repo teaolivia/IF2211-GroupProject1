5128958634fc2e1bb50b6a9bfdb06d946a47cd1c
Thea Olivia <theaolivia.mail@gmail.com>
2015-02-28 00:43:53 -0800

    Create Greedy.cpp

---
+++ Greedy.cpp
@@ -0,0 +1,39 @@
+#include "Greedy.h"
+
+Greedy::~Greedy(){};
+
+Greedy::Greedy()
+{
+	int i,j; // untuk iterasi
+	Release = 0; // awal dari pemakaian
+	Deadline = 0; // batas akhir pemakaian
+	char Barang[] = "kulkas","TV","AC","Mesin Cuci", "" ;
+	float kWh[] // array kosong berisi besar daya yang akan dibaca melalui teks
+	int on[]; // array kosong untuk menyimpan waktu menyala 
+	int off[]; // array kosong untuk menyimpan waktu mati
+	int NSlot[]; // array kosong untuk menyimpan slot yang kosong
+	Priority = 0; // Kepentingan pemakaian listrik
+}
+	// atribut
+	void Greedy::InitGreedy(){
+		int i;
+		i = 0;
+	}
+	void Greedy::GreedykWh(){ // greedy untuk mencari nilai kWH terbanyak
+		int k;
+
+	};
+
+	void Greedy::GreedyMoney(){
+		int N_Slot;
+		for (N_Slot=0; N_Slot < MaxSlot; N_Slot++){
+			Path[N_Slot] = StartSlot;
+		}
+	}; 
+
+	void Greedy::GreedyPriority(){
+		int N_Slot;
+		for (N_Slot=0; N_Slot < MaxSlot; N_Slot++)
+	}
+
+	void AI::
